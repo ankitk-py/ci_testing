2bcd6235f8cf8c1e175fb406ad8be75e39e5de6a
“Ankit <ankitkhedkar1994@gmail.com>
2023-09-10 11:28:21 +0530

    code added

---
+++ app.py
@@ -0,0 +1,14 @@
+
+def addition():
+    '''
+    adds the two numbers 
+    '''
+
+    x_value = 10
+    y_value = 20
+
+    return x_value + y_value
+
+
+addition()
+
---
+++ test_app.py
@@ -0,0 +1,6 @@
+from app import addition
+
+def test_addition():
+    '''testing of app.py module'''
+
+    assert addition() == 30
