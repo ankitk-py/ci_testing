4849a7babb69b84659e4d6ecaad56353ff9186aa
“Ankit <ankitkhedkar1994@gmail.com>
2023-09-10 13:04:44 +0530

    corrected flake8 errors

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-
 def addition():
-    '''
-    adds the two numbers 
-    '''
 
+    '''
+    adds the two numbers
+    '''
     x_value = 10
     y_value = 20
-
     return x_value + y_value
 
 
 addition()
-
